845892f6a4355659cb64cf5914d752ecb064e45f
FuXin-1024 <1007160080@qq.com>
2017-05-16 20:40:44 +0800

    消息队列

---
+++ m_queue/comm.h
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: comm.h
+	> Author: fx
+	> Mail: 1007160080@qq..com 
+	> Created Time: Tue 16 May 2017 01:34:07 PM PDT
+ ************************************************************************/
+
+#ifndef __COMM__
+#define __COMM__
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<sys/types.h>
+#include<string.h>
+#include<time.h>
+
+#define __MSG_SIZE__ 1024
+#define FILEPATH 
