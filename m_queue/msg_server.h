2c601fd3aad9831ee4f63d1e6fd562e29db3e144
FuXin-1024 <1007160080@qq.com>
2017-05-17 14:52:53 +0800

    msg_server.h

---
+++ m_queue/msg_server.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: msg_server.h
+	> Author: fx
+	> Mail: 1007160080@qq..com 
+	> Created Time: Wed 17 May 2017 07:49:12 AM PDT
+ ************************************************************************/
+
+
+#ifndef _MSG_SERVER_
+#define _MSG_SERVER_
+
+#include"comm.h"
+
+int msg_server_start();
+int msg_server_start(int);
+
+#endif
