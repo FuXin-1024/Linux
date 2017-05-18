cbd3e011894d8abb6af418b900de9b2922332945
FuXin-1024 <1007160080@qq.com>
2017-05-18 20:35:06 +0800

    comm.c

--- m_queue/comm.c
+++ m_queue/comm.c
@@ -1,17 +1,77 @@
 /*************************************************************************
 	> File Name: comm.c
 	> Author: fx
-	> Mail: 1007160080@qq..com 
-	> Created Time: Wed 17 May 2017 07:43:59 AM PDT
+	> Mail: 1007160080@qq.com 
+	> Created Time: Thu 18 May 2017 01:23:51 PM PDT
  ************************************************************************/
 
-#include<stdio.h>
 #include"comm.h"
 
-const int g_ser_send_type=1;//serve
-const int g_cli_send_type=2;//client
-
-void print_log(char* msg)
+int commMsgQueue(int flag)
 {
-	print("%s[%d]:%s\n",__FUNCTION__,__LINE__,msg);
+	 key_t _key = ftok(PATHNAME,PROJID);
+     if(_k < 0)
+	 {
+		  perror("ftok");
+		   return -1;
+								    \
+	 }
+	 int msg_id = msgget(_key,flag);
+	 if(msg_id <0)
+	 {
+		 perror("msgget");
+		 return -2;
+	 }
+			 
+	 return msg_id;
+}
+
+
+int creatMsgQueue()
+{
+	    return commMsgQueue(IPC_CREAT | IPC_EXCL | 0666);
+}
+int getMsgQueue()
+{
+	    return commMsgQueue(IPC_CREAT);
+}
+
+int sendMessage(int msg_id,long type, const char* msg)
+{
+	    struct msgbuf buf;
+		 buf.mtype = type;
+	     strcpy(buf.mtext,msg);
+
+		 int ret = msgsnd(msg_id,&buf,sizeof(buf.mtext),0);
+	     if(ret < 0)
+		 {
+			 perror("msgsnd");
+			 return -1;
+		 }
+			 return 0;
+}
+int recvMessage(int msg_id,long type,char out[])
+{
+	    struct msgbuf buf;
+		 int size = msgrcv(msg_id,&buf,sizeof(buf.mtext),type,0);
+		 if(size > 0)
+	     {
+		  printf("%d",size);
+		  buf.mtext[size-1] = '\0';
+		  printf("Debug 0 : %s\n",buf.mtext);
+		  strncpy(out,buf.mtext,sizeof(buf.mtext));
+		  return size;
+         }
+		 perror("msgrcv");
+		 return -1;
+						
+}
+int destroyMsgQueue(int msg_id)
+{
+	 if(msgctl(msg_id,IPC_RMID,NULL) < 0)
+	 {
+		 perror("msgctl");
+		 return -1;
+	  }
+		 return 0;
 }
