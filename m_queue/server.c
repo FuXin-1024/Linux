497b9fae8fff6c269ce90d8087be0764d38c30d8
FuXin-1024 <1007160080@qq.com>
2017-06-13 18:52:55 +0800

    server.c

--- m_queue/server.c
+++ m_queue/server.c
@@ -1,36 +1,35 @@
 /*************************************************************************
 	> File Name: server.c
 	> Author: fx
-	> Mail: 1007160080@qq.com 
-	> Created Time: Thu 18 May 2017 01:59:43 PM PDT
+	> Mail: 1007160080qq.com 
+	> Created Time: Tue 13 Jun 2017 11:37:30 AM PDT
  ************************************************************************/
-//发送端
+
 #include"comm.h"
 
 int main()
 {
-	int msgid=creatMsgQueue();
-	char buf[MSG_SIZE*2];
-
+	int msgid = creatMsgQueue();
+	char buf[MYSIZE*2];
 	while(1)
 	{
 		int size = recvMessage(msgid,CLIENT_TYPE,buf);
-		 printf("DEBUG: %d\n",size);
-		 if(size > 0)
-		 {
-			 buf[size] = '\0';
-			 printf("client say: %s\n",buf);
-			 printf("Debug : %s\n",buf);
-		 }
-		 else
-		 {
-			 break;
-		 }
-	     int ret = sendMessage(msgid,SERVER_TYPE,buf);
-		 if(ret < 0)
-		 {
-			 break;
-		 }
+		printf("DEBUG: %d\n",size);
+		if(size>0)
+		{
+			buf[size]='\0';
+			printf("client say: %s\n",buf);
+			printf("Debug: %s\n",buf);
+		}
+		else
+		{
+			break;
+		}
+		
+		int ret = sendMessage(msgid,SERVER_TYPE,buf);
+		if(ret<0)
+			break;
+
 	}
 	destroyMsgQueue(msgid);
 	return 0;
