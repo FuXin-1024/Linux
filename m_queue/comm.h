d8bf6555958d6892ef3249ad1754c74743cf4fc4
FuXin-1024 <1007160080@qq.com>
2017-06-13 18:08:04 +0800

    comm.h

--- m_queue/comm.h
+++ m_queue/comm.h
@@ -1,41 +1,39 @@
 /*************************************************************************
 	> File Name: comm.h
 	> Author: fx
-	> Mail: 1007160080@qq..com 
-	> Created Time: Tue 16 May 2017 01:34:07 PM PDT
+	> Mail: 1007160080qq.com 
+	> Created Time: Tue 13 Jun 2017 10:57:17 AM PDT
  ************************************************************************/
 
-#ifndef __COMM__
-#define __COMM__
+#ifndef _COMM_
+#define _COMM_
 
 #include<stdio.h>
-#include<stdlib.h>
+#include<sys/types.h>
 #include<sys/ipc.h>
 #include<sys/msg.h>
-#include<sys/types.h>
 #include<string.h>
-#include<time.h>
 #include<unistd.h>
+#define MYSIZE 128
 
-#define MSG_SIZE 1024
 #define PATHNAME "."
 #define PROJID 0x6666
 
 #define CLIENT_TYPE 1
 #define SERVER_TYPE 2
 
-
- strut _msgbuf
+struct msgbuf
 {
 	long mtype;
-	char mtext[MSG_SIZE];
-}msginfo;
+	char mtext[MYSIZE];
+};
 
 int commMsgQueue(int flag);
-int creatMasQueue();
+int creatMsgQueue();
 int getMsgQueue();
-int sendMessage(int msgid,long type,const char* buf);
-inr recvMessage(int msgid,long type,char* buf);
+int sendMessage(int msgid.long type,const char *buf);
 int destroyMsgQueue(int msgid);
 
-#endif;
+#endif
+
+
