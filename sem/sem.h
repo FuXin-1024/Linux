810b71c2a7d4c6c650a9ac8bedab649508bed5cb
FuXin-1024 <1007160080@qq.com>
2017-06-14 21:28:16 +0800

    sem.h

--- sem/sem.h
+++ sem/sem.h
@@ -8,19 +8,20 @@
 #ifndef _SEM_H_
 #define _SEM_H_
 
-#define PATHNAME","
+#define PATHNAME "."
 #define PROJ_ID 0x6666
-
+#include<stdio.h>
 #include<sys/types.h>
 #include<sys/ipc.h>
 #include<sys/sem.h>
+
 union semun
 {
 	int val;                   //使用的值
 	struct semid_ds *buf;      //IPC_STAT、IPC_SET  使用缓存
 	unsigned short *array;     //AETALL、SETALL     使用的数组
 	struct seminfo *__buf;	   //IPC_INFO(Linux特有)使用缓存区
-}；
+};
 
 int CreateSem(int nums);
 int DestroySem(int semid);
