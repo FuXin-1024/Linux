72fd59c16f4a6e0e82c1a6c121a2f2c01072e878
FuXin-1024 <1007160080@qq.com>
2017-06-20 19:01:14 +0800

    共享内存comm.h

---
+++ shm/comm.h
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: comm.h
+	> Author: fx
+	> Mail: 1007160080qq.com 
+	> Created Time: Tue 20 Jun 2017 11:56:28 AM PDT
+ ************************************************************************/
+
+#ifndef __COMM__
+#define __COMM_
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/sem.h>
+
+#define PATHNAME "."
+#define PROJID 0x666
+#define SIZE 4096
+
+int commMem(int flags);
+int creatMem();
+int getMem();
+int destroyMem(int shmid);
+
+#endif
