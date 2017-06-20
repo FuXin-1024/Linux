b5f3a43ea2497a319842150908620ba0eab8198a
FuXin-1024 <1007160080@qq.com>
2017-06-20 19:22:46 +0800

    共享内存client.c

---
+++ shm/client.c
@@ -0,0 +1,49 @@
+/*************************************************************************
+	> File Name: client.c
+	> Author: fx
+	> Mail: 1007160080qq.com 
+	> Created Time: Tue 20 Jun 2017 12:13:59 PM PDT
+ ************************************************************************/
+
+#include"comm.h"
+
+int main()
+{
+	printf("client\n");
+	int shmid = getMem();
+	if(shmid < 0)
+	{
+		printf("getMem error!");
+		return -1;
+	}
+	printf("shmid: %d\n",shmid);
+	sleep(5);
+	char* addr=shmat(shmid,NULL,0);
+	if(addr == NULL)
+	{
+		printf("addr is NULL!\n");
+	}
+	if(addr < 0)
+	{
+		perror("client shmat");
+		return -1;
+	}
+
+	int i = 0;
+	while(i < SIZE-1)
+	{
+		addr[i] = i;
+		addr[i+1] = '\0';
+		sleep(1);
+		i++;
+	}
+
+	int ret=shmdt(addr);
+	if(ret < 0)
+	{
+		perror("client shmdt");
+		return -1;
+	}
+	return 0;
+}
+
