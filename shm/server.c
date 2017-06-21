2fd3a9f394887bec39929329276a04ecc73e833e
FuXin-1024 <1007160080@qq.com>
2017-06-21 19:15:35 +0800

    共享内存server.c

--- shm/server.c
+++ shm/server.c
@@ -9,15 +9,16 @@
 
 int main()
 {
+	printf("server\n");
 	int shmid = creatMem();
-	slepp(10);
+	sleep(10);
 	if(shmid < 0)
 	{
 		printf("creatMem error!\n");
 		return -1;
 	}
 
-	char* addr = shmat(shmid,NULL,0);
+	char *addr=shmat(shmid,NULL,0);
 	if(addr < 0)
 	{
 		perror("shmat");
@@ -25,9 +26,9 @@ int main()
 	}
 	while(1)
 	{
-		printf("#\n");
-		printf("%s\n",addr);
-		printf("#\n");
+		printf("# ");
+		printf("%s",addr);
+		printf(" #\n");
 		sleep(1);
 	}
 
@@ -37,11 +38,13 @@ int main()
 		perror("shmdt");
 		return -1;
 	}
-
+/*
 	int ret_des=destroyMem(shmid);
 	if(ret_des < 0)
 	{
 		printf("destroyMem error!\n");
 		return -1;
 	}
-	return 0
+*/
+	return 0;
+}
