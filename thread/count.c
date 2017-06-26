75bc37769f7ac583b1a29eec3d09aac882de316b
FuXin-1024 <1007160080@qq.com>
2017-06-26 15:28:49 +0800

    枷锁后的，创建两个线程count各加5000次

--- thread/count.c
+++ thread/count.c
@@ -10,15 +10,18 @@
 #include<pthread.h>
 
 static int g_count = 0;
+pthread_mutex_t mutex_lock = PTHREAD_MUTEX_INITIALIZER;
 void* read_write_num(void* _val)
 {
 	int val = 0;
 	int i = 0;
 	for(; i<5000; ++i)
 	{
+		pthread_mutex_lock(&mutex_lock);
 		val  = g_count;
 		printf("pthread id is:%x,count is : %d\n",(unsigned long)pthread_self(),g_count);
 		g_count = val + 1;
+		pthread_mutex_unlock(&mutex_lock);
 	}
 	return NULL;
 }
