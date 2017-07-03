9c1881f7d3147f6a53ae5d29598cba2d7d2bac10
FuXin-1024 <1007160080@qq.com>
2017-07-03 14:55:10 +0800

    模拟实现mysleep函数

--- signal/mysleep.c
+++ signal/mysleep.c
@@ -5,6 +5,7 @@
 	> Created Time: Sun 02 Jul 2017 03:39:37 AM PDT
  ************************************************************************/
 
+
 #include<stdio.h>
 #include<signal.h>
 #include<unistd.h>
@@ -18,16 +19,16 @@ int mysleep(int secs)
 {
 	struct sigaction sig,osig;
 	//设置sugaction的属性
-	sig.sa_handler = sig_run;
-	sigemptyset(&sig.sa_mask);
+	sig.sa_handler = sig_run;//设置处理函数
+	sigemptyset(&sig.sa_mask);//将表示阻塞的标记初始化为零
 	sig.sa_flags = 0;
 	sigaction(SIGALRM,&sig,&osig);
 
-	alarm(secs);
-	pause();
-	int ret = alarm(0);
+	alarm(secs);//设置闹钟，等待闹钟secs秒后，发送SIGALRM信号
+	pause();//进程挂起直到有信号递达
+	int ret = alarm(0);//清除闹钟，并保存函数返回值，与sleep函数值一致
 
-	sigaction(SIGALRM,&osig,NULL);
+	sigaction(SIGALRM,&osig,NULL);//将SIGALRM信号的处理方式设置成osig
 	return ret;
 }
 
