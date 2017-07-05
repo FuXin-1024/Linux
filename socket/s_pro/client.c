c82b0a2f94388030e63954ec2f3952df377bf1c8
FuXin-1024 <1007160080@qq.com>
2017-07-05 16:34:48 +0800

    单进程套接字client.c

---
+++ socket/s_pro/client.c
@@ -0,0 +1,65 @@
+/*************************************************************************
+	> File Name: client.c
+	> Author: fx
+	> Mail: 1007160080qq.com 
+	> Created Time: Wed 05 Jul 2017 09:20:38 AM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+#include<sys/socket.>
+#include<sys/types.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+
+
+
+void Usage(char* name)
+{
+	printf("Usage:%s [IpAddress] [port]\n",name);
+}
+
+
+int main(int argc,const char* argv[])
+{
+	if(argc != 3)
+	{
+		Usage(argv[0]);
+		return 1;
+	}
+
+	int sock = socket(AF_INET,SOCK_STREAM,0);
+	if(sock < 0)
+	{
+		perror("socke");
+		return 2;
+	}
+
+	struct sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(atoi(argv[2]));
+	server.sin_addr.s_addr = inet_addr(argv[1]);
+
+	if(connect(sock,(struct sockaddr* )&server,sizeof(server)) < 0);
+	{
+		perror("connect");
+		return 3;
+	}
+
+	char buf[1024];
+	while(1)
+	{
+		printf("send# ");
+		fflush(stdout);
+		ssize_t s = read(0,buf,sizeof(buf)-1);
+		if(s < 0)
+		{
+			perror("read");
+			return 4;
+		}
+		buf[s-1] = 0;
+		write(sock,buf,s);
+	}
+	close(sock);
+	return 0;
+}
+
