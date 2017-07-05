803ebe3e818e49dc45882ebd3b5cc2249c695eab
FuXin-1024 <1007160080@qq.com>
2017-07-05 18:10:31 +0800

    单进程套接字client.c

--- socket/s_pro/client.c
+++ socket/s_pro/client.c
@@ -8,9 +8,10 @@
 #include<stdio.h>
 #include<sys/socket.h>
 #include<sys/types.h>
+#include<netinet/in.h>
 #include<unistd.h>
 #include<arpa/inet.h>
-
+#include<string.h>
 
 
 void Usage(const char* name)
@@ -39,25 +40,43 @@ int main(int argc,const char* argv[])
 	server.sin_port = htons(atoi(argv[2]));
 	server.sin_addr.s_addr = inet_addr(argv[1]);
 
-	if(connect(sock,(struct sockaddr* )&server,sizeof(server)) < 0);
+	if(connect(sock,(struct sockaddr*)&server,sizeof(server)) < 0)
 	{
 		perror("connect");
 		return 3;
 	}
 
+	//printf("connect success!\n");
 	char buf[1024];
 	while(1)
 	{
 		printf("send# ");
 		fflush(stdout);
 		ssize_t s = read(0,buf,sizeof(buf)-1);
-		if(s < 0)
+		if(s <= 0)
 		{
 			perror("read");
 			return 4;
 		}
 		buf[s-1] = 0;
-		write(sock,buf,s);
+		write(sock,buf,strlen(buf));
+
+		ssize_t _s = read(sock, buf, sizeof(buf)-1);
+		if(_s == 0)
+		{
+			printf("server quit\n");
+			break;
+		}
+		else if(_s < 0)
+		{
+			perror("read");
+			return 5;
+		}
+		else
+		{
+			buf[s]=0;
+			printf("server #: %s\n", buf);
+		}
 	}
 	close(sock);
 	return 0;
