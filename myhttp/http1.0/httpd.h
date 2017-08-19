b12ce13ae6ac9ad6fcd333f8bad3cdc8a7057b4a
FuXin-1024 <1007160080@qq.com>
2017-08-19 09:12:57 +0800

    httpd1.0

---
+++ myhttp/http1.0/httpd.h
@@ -0,0 +1,33 @@
+/*************************************************************************
+	> File Name: httpd.h
+	> Author: fx
+	> Mail: 1007160080qq.com 
+	> Created Time: Fri 18 Aug 2017 07:43:26 AM PDT
+ ************************************************************************/
+
+#ifndef __HTTPD_H_
+#define __HTTPD_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<pthread.h>
+#include<assert.h>
+#include<ctype.h>
+#include<string.h>
+
+#define WARNING 1
+#define FATAL 2
+#define SZIE 102400
+
+void print_log(const char* str,int error);
+int startup(const char* ip,const char* port);
+int get_line(int fd,char* buf,int size);
+void Dealwithsock(int sock);
+
+#endif
+
