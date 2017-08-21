b2b7057ae0c055d4cb4295fb3d979a25e5db3aeb
FuXin-1024 <1007160080@qq.com>
2017-08-21 10:28:12 +0800

    httpd3.0

---
+++ myhttp/http3.0/cgi/cgifun.c
@@ -0,0 +1,52 @@
+/*************************************************************************
+	> File Name: cgifun.c
+	> Author: fx
+	> Mail: 1007160080qq.com 
+	> Created Time: Sat 19 Aug 2017 10:11:59 PM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+	printf("<html>\n");
+	printf("<h1>nihao</h1></html>\n");
+
+	char* method = getenv("METHOD");
+	printf("%s\n",method);
+	char first[255] = {0};
+	char last[255] = {0};
+	char data[512] = {0};
+
+	if(method == NULL)
+	{
+		printf("method is error\n");
+		return -1;
+	}
+
+	int i = 0;
+	if(strcmp("GET",method) == 0)
+	{
+		char* query = getenv("QUERY_STRING");
+		strcpy(data,query);
+		printf("%s\n",query);
+	}
+
+	if(strcmp(method,"POST") == 0)
+	{
+		char ch = '\0';
+		int sz = 0;
+		sz = recv(0,&ch,1,0);
+
+		while(se > 0)
+		{
+			if(ch == '=')
+			{}
+		}
+	}
+
+	printf("<html> hello world <html>\n");
+	return 0;
+}
